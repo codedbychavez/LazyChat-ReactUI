76875a1f22aa16ad92d886bee357b87be81e42ba
codedbychavez <codedbychavez@gmail.com>
2022-03-18 16:06:22 -0400

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 import styled from "styled-components";
 
-import ChatWindow from "./components/organisms/ChatWindow"
+import ChatWindow from "./components/organisms/ChatWindow";
 
 const App = () => {
   return (
     <AppWrapper>
       <ChatWindow />
     </AppWrapper>
-
-  )
-}
+  );
+};
 
 const AppWrapper = styled(Container)`
   margin: 5rem;
-`
+`;
 
 export default App;
